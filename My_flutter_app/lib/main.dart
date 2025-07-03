dee92dd47830e783648373ce54b8d4a49dc3bd3d
Waqasho20 <waqasrk27@outlook.com>
2025-07-03 10:35:32 +0500

    Update main.dart

--- My_flutter_app/lib/main.dart
+++ My_flutter_app/lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Hello Waqas',
       home: Scaffold(
-        appBar: AppBar(title: Text('Codemagic App')),
-        body: Center(child: Text('Hello from Codemagic!')),
+        appBar: AppBar(
+          title: Text('Waqas Flutter App'),
+        ),
+        body: Center(
+          child: Text('Hello, Waqas!'),
+        ),
       ),
     );
   }
